d6406fb6e13cc9f295cf0b38b2158d0a5540bbcf
Alejandro Pons <alejandro.pons@correo.unimet.edu.ve>
2022-10-23 00:15:11 -0400

    Trying to implement card grid

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,12 @@ import { useEffect } from 'react';
 
 function App() {
   const [character, setCharacter] = useState('');
+  const [characters, setCharacters] = useState([]);
 
   function getCharacter(id) {
-    
+
     const url = 'https://localhost:7182/api/character/' + id;
-    
+
     useEffect(() => {
       fetch(url, {
         method: 'GET',
@@ -23,29 +24,56 @@ function App() {
     }, []);
   }
 
+  function getCharacters() {
+    const url = 'https://localhost:7182/api/character/getall';
+
+    useEffect(() => {
+      fetch(url, {
+        method: 'GET',
+      })
+        .then(res => res.json())
+        .then(res => {
+          setCharacters(res.data);
+          console.log(res);
+        })
+        .catch(err => console.log(err));
+    }, []);
+  }
+
   return (
+    // getCharacters(),
+    //       <div className='row row-cols-1 row-cols-md-2 g-4'>
+    //         {characters.map((character) => (
+    //           <div className='col'>
+    //             <div className='card h-100'>
+    //             <img src={character.imgUrl} className='card-image-top' alt='...' />
+    //             <div className='card-body'>
+    //               <h5 className='card-title'>{character.name}</h5>
+    //               <p className='card-text'>Devil Fruit: {character.devilFruit}</p>
+    //             </div>
+    //           </div>
+    //           </div>
+                
+    //         ))}
+    // </div>
     getCharacter(1),
     <div className='container'>
-      <div className='row min-vh-100'>
-        <div className='col-12 d-flex flex-column justify-content-center align-items-center'>
           {renderCharacter(character)}
-        </div>
-      </div>
     </div>
-  );
+        );
 
-  function renderCharacter() {
+        function renderCharacter() {
     return (
-    <div className="card">
-      <img className="card-img-top" src={character.imageUrl} alt="Card image cap" />
-      <div className="card-body">
-        <h1 className="card-title">{character.name}</h1>
-        <h5 className="card-text">Devil Fruit: {character.devilFruit}</ h5>
-        <h5 className="card-text">Pirate Crew: {character.pirateCrew}</ h5>
-      </div>
-    </div>
-    );
+        <div className="card">
+          <img className="card-img-top" src={character.imageUrl} alt="Card image cap" />
+          <div className="card-body">
+            <h1 className="card-title">{character.name}</h1>
+            <h5 className="card-text">Devil Fruit: {character.devilFruit}</ h5>
+            <h5 className="card-text">Pirate Crew: {character.pirateCrew}</ h5>
+          </div>
+        </div>
+        );
   }
 }
 
-export default App
+        export default App
